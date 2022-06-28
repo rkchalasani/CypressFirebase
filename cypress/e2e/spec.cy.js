6c5941ff0fac9e3fde73bbd7b48b2509d9b63d70
Ramakrishna Rao Chalasani <ramakrishna.chalasani@rampgroup.com>
2022-06-28 17:10:57 +0530

    made changes

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -6,10 +6,10 @@ describe('empty spec', () => {
     // cy.get("#signuppass").type("123456");
     // cy.get("#signup").click();
     // cy.get('#signin').click();
-    cy.get("#input-email").type("test@gmail.com");
-    cy.get("#input-pass").type("test123");
-    cy.get("#signinbutton").click();
-    cy.get("#useremail").contains("test@gmail.com");
-    cy.get("#logoutbutton").click();
+    // cy.get("#input-email").type("test@gmail.com");
+    // cy.get("#input-pass").type("test123");
+    // cy.get("#signinbutton").click();
+    // cy.get("#useremail").contains("test@gmail.com");
+    // cy.get("#logoutbutton").click();
   })
 })
